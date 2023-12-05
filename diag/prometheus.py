1e2eeafc0dbe42455d2f1e6ec3c4de6914135ff8
zwang28 <84491488@qq.com>
2023-12-05 23:19:24 +0800

    add promql_filters argument

--- diag/prometheus.py
+++ diag/prometheus.py
@@ -1,6 +1,6 @@
 from prometheus_api_client import PrometheusConnect
 
-def diag(prometheus_address):
+def diag(prometheus_address, promql_filters):
   """
   Query prometheus and (TODO)raise alarm.
   """
@@ -13,32 +13,32 @@ def diag(prometheus_address):
   print("\n>>diag prometheus")
   prometheus = PrometheusConnect(url=f'http://{prometheus_address}')
 
-  metric_data = prometheus.custom_query(query='topk(3, rate(stream_source_output_rows_counts[10m]))')
+  metric_data = prometheus.custom_query(query=f'topk(3, rate(stream_source_output_rows_counts{{{promql_filters}}}[10m]))')
   print(f"\nSource Throughput(rows/s)\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='topk(3, sum(rate(stream_mview_input_row_count[10m])) by (table_id) * on(table_id) group_left(table_name) group(table_info) by (table_id, table_name))')
+  metric_data = prometheus.custom_query(query=f'topk(3, sum(rate(stream_mview_input_row_count{{{promql_filters}}}[10m])) by (table_id) * on(table_id) group_left(table_name) group(table_info) by (table_id, table_name))')
   print(f"\nMaterialized View Throughput(rows/s)\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='topk(3, histogram_quantile(0.9, sum(rate(stream_join_matched_join_keys_bucket[10m])) by (le, fragment_id, table_id)))')
+  metric_data = prometheus.custom_query(query=f'topk(3, histogram_quantile(0.9, sum(rate(stream_join_matched_join_keys_bucket{{{promql_filters}}}[10m])) by (le, fragment_id, table_id)))')
   print(f"\nJoin Executor Matched Rows\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='topk(3, histogram_quantile(0.5, sum(rate(state_store_get_duration_bucket[10m])) by (le, table_id)))')
+  metric_data = prometheus.custom_query(query=f'topk(3, histogram_quantile(0.5, sum(rate(state_store_get_duration_bucket{{{promql_filters}}}[10m])) by (le, table_id)))')
   print(f"\nHummock Read Duration - Get\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='topk(3, histogram_quantile(0.5, sum(rate(state_store_iter_init_duration_bucket[10m])) by (le, table_id)))')
+  metric_data = prometheus.custom_query(query=f'topk(3, histogram_quantile(0.5, sum(rate(state_store_iter_init_duration_bucket{{{promql_filters}}}[10m])) by (le, table_id)))')
   print(f"\nHummock Read Duration - Iter\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='topk(3, sum(rate(storage_commit_write_throughput[10m])) by (table_id))')
+  metric_data = prometheus.custom_query(query=f'topk(3, sum(rate(storage_commit_write_throughput{{{promql_filters}}}[10m])) by (table_id))')
   print(f"\nCommit Flush Bytes\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='sum(rate(object_store_read_bytes{job=~"compute|compactor"}[10m]))by(job, instance)')
+  metric_data = prometheus.custom_query(query=f'sum(rate(object_store_read_bytes{{{promql_filters},job=~"compute|compactor"}}[10m]))by(job, instance)')
   print(f"\nObject Store Read Throughput Bytes\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='sum(rate(object_store_write_bytes{job=~"compute|compactor"}[10m]))by(job, instance)')
+  metric_data = prometheus.custom_query(query=f'sum(rate(object_store_write_bytes{{{promql_filters},job=~"compute|compactor"}}[10m]))by(job, instance)')
   print(f"\nObject Store Write Throughput Bytes\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='sum(rate(object_store_operation_latency_count{job=~"compute|compactor", type!~"streaming_upload_write_bytes|streaming_read_read_bytes|streaming_read"}[10m])) by (le, type, job, instance)')
+  metric_data = prometheus.custom_query(query=f'sum(rate(object_store_operation_latency_count{{{promql_filters},job=~"compute|compactor", type!~"streaming_upload_write_bytes|streaming_read_read_bytes|streaming_read"}}[10m])) by (le, type, job, instance)')
   print(f"\nObject Store Operation Rate\n{metric_data}")
 
-  metric_data = prometheus.custom_query(query='histogram_quantile(0.5, sum(rate(object_store_operation_latency_bucket{job=~"compute|compactor", type!~"streaming_upload_write_bytes|streaming_read"}[10m])) by (le, type, job, instance))')
+  metric_data = prometheus.custom_query(query=f'histogram_quantile(0.5, sum(rate(object_store_operation_latency_bucket{{{promql_filters},job=~"compute|compactor", type!~"streaming_upload_write_bytes|streaming_read"}}[10m])) by (le, type, job, instance))')
   print(f"\nObject Store Operation Duration\n{metric_data}")
--- run.py
+++ run.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
                     prog='risingwave diagnose')
   parser.add_argument('--connection_string', help='https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING')
   parser.add_argument('--prometheus_address', help='host:port')
+  parser.add_argument('--promql_filters', default='job!=""')
   args = parser.parse_args()
 
   risingwave.diag(args.connection_string)
-  prometheus.diag(args.prometheus_address)
+  prometheus.diag(args.prometheus_address, args.promql_filters)
   await_tree.diag(args.connection_string)
